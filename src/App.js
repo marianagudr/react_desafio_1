f7537bab2bb05dfa3bbd9ea6accf3784237b9110
Mariana Guevara <marianagudr@gmail.com>
2022-03-18 19:32:19 -0300

    Primera carga de archivos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola! Este es mi desafio 1
         </p>
         <a
           className="App-link"
