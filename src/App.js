0fa45c8cd6b3d34226422d0fcfbebdaa5322a97b
Mariana Guevara <marianagudr@gmail.com>
2022-03-27 13:29:38 -0300

    Primera carga de archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import Body from './components/Body.js';
 
-function App() {
+const App = ()  => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola! Este es mi desafio 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Body />
     </div>
-  );
+  )
 }
 
 export default App;
